6c1440cee166edb4b9d05d66614ca5cc193c6a5f
Christian Azul <53144221+ChristianAzulL@users.noreply.github.com>
2025-01-19 20:48:39 +0800

    Update notifications.php

--- Inventory Management System/page_properties/notifications.php
+++ Inventory Management System/page_properties/notifications.php
@@ -3,6 +3,7 @@ include "../config/database.php";
 include "../config/on_session.php";
 
 $notification1 = "SELECT
+                    p.product_img,
                     p.description,
                     p.safety,
                     p.hashed_id,
@@ -16,6 +17,7 @@ $result1 = $conn->query($notification1);
 if ($result1->num_rows > 0) {
     while ($row1 = $result1->fetch_assoc()) { // Corrected from $result to $result1
         $not_product_description = $row1['description'];
+        $not_product_img = $row['product_img'] ?? '../../assets/img/def_img.png';
         $not_safety = $row1['safety'];
         $not_product_id = $row1['hashed_id'];
         $not_brand_name = $row1['brand_name'];
@@ -25,17 +27,20 @@ if ($result1->num_rows > 0) {
 
         // Query to fetch stock information
         $notification2 = "SELECT
-                            item_status,
-                            `date`
-                          FROM stocks
-                          WHERE warehouse IN ($user_warehouse_id)
-                          AND product_id = '$not_product_id'";
+                            s.item_status,
+                            s.date,
+                            w.warehouse_name
+                          FROM stocks s
+                          LEFT JOIN warehouse w ON w.hashed_id = s.warehouse
+                          WHERE s.warehouse IN ($user_warehouse_id)
+                          AND s.product_id = '$not_product_id'";
         $result2 = $conn->query($notification2);
 
         if ($result2->num_rows > 0) {
             while ($row2 = $result2->fetch_assoc()) { // Corrected from $result to $result2
                 $not_item_status = $row2['item_status'];
                 $not_date_added = $row2['date'];
+                $not_warehouse = $row2['warehouse_name'];
 
                 // Increment quantity for specific item statuses
                 if ($not_item_status == 0 || $not_item_status == 2 || $not_item_status == 3) {
@@ -46,6 +51,9 @@ if ($result1->num_rows > 0) {
                 $not_dateTime = new DateTime($not_date_added);
                 $not_now = new DateTime(); // Get the current date and time
 
+                // Format the date to "January 1, 2020"
+                $formatted_date_added = $not_dateTime->format('F j, Y');
+
                 // Calculate the difference between the dates
                 $not_interval = $not_dateTime->diff($not_now);
 
@@ -55,23 +63,24 @@ if ($result1->num_rows > 0) {
                 // Generate notifications based on total months
                 if ($not_totalMonths >= 1 && $not_totalMonths < 2) {
                     if ($not_item_status == 0 || $not_item_status == 2 || $not_item_status == 3) {
-                        echo "<br>Description: $not_product_description<br>Brand: $not_brand_name<br>Category: $not_category_name<br>is now more than 1 month on our store since $not_date_added";
+                        echo "<br>Product_image: " . $not_product_img .  "<br>Description: $not_product_description<br>Brand: $not_brand_name<br>Category: $not_category_name<br>is now more than 1 month on our store since $formatted_date_added";
                     }
                 } elseif ($not_totalMonths >= 2 && $not_totalMonths < 3) {
                     if ($not_item_status == 0 || $not_item_status == 2 || $not_item_status == 3) {
-                        echo "<br>Description: $not_product_description<br>Brand: $not_brand_name<br>Category: $not_category_name<br>is now more than 2 months on our store since $not_date_added";
+                        echo "<br>Product_image: " . $not_product_img .  "<br>Description: $not_product_description<br>Brand: $not_brand_name<br>Category: $not_category_name<br>is now more than 2 months on our store since $formatted_date_added";
                     }
                 } elseif ($not_totalMonths >= 3) {
                     if ($not_item_status == 0 || $not_item_status == 2 || $not_item_status == 3) {
-                        echo "<br>Description: $not_product_description<br>Brand: $not_brand_name<br>Category: $not_category_name<br>is now more than 3 months on our store since $not_date_added";
+                        echo "<br>Product_image: " . $not_product_img .  "<br>Description: $not_product_description<br>Brand: $not_brand_name<br>Category: $not_category_name<br>is now more than 3 months on our store since $formatted_date_added";
                     }
                 }
+
             }
         }
 
         // Safety level notification
         if ($notification_item_quantity <= $not_safety) {
-            echo "<br>---------------------------<br>Description: $not_product_description<br>Brand: $not_brand_name<br>Category: $not_category_name<br>Quantity: $notification_item_quantity<br>Note: Quantity is less than the safety level.<br>-------------------------------";
+            echo "<br>Product_image: " . $not_product_img .  "<br>---------------------------<br>Description: $not_product_description<br>Brand: $not_brand_name<br>Category: $not_category_name<br>Quantity: $notification_item_quantity<br>Note: Quantity is less than the safety level.<br>-------------------------------";
         }
     }
 }
