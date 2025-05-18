aed58c210dd335d4ce1938c4473d8a970cc88eb4
Christian Azul <53144221+ChristianAzulL@users.noreply.github.com>
2025-05-19 01:24:26 +0800

    incoming stocks module update
    completed. testing has done already. data is accurate. not yet tested on multiple products(such as more than 3 products)

--- Inventory Management System/dashboard/incoming_stocks.php
+++ Inventory Management System/dashboard/incoming_stocks.php
@@ -2,7 +2,9 @@
     <div class="card-body">
         <div class="row flex-between-center g-0">
             <div class="col-auto mb-3">
-                <h6 class="mb-0">Incoming Stocks</h6>
+                <h6 class="mb-0">
+                    <a href="../incoming-stocks/?wh=<?php echo $dashboard_wh; ?>">Incoming Stocks</a>
+                </h6>
             </div>
             <div class="col-12">
                 <div class="table-responsive">
@@ -37,6 +39,18 @@
                                         AND s.warehouse IN ($imploded_warehouse_ids)
                                         GROUP BY p.category
                                         ";
+
+                                        $po_query = "
+                                            SELECT 
+                                                SUM(poc.qty) AS incoming
+                                            FROM purchased_order_content poc
+                                            LEFT JOIN purchased_order po ON po.id = poc.po_id
+                                            LEFT JOIN product p ON p.hashed_id = poc.product_id
+                                            LEFT JOIN category c ON c.hashed_id = p.category
+                                            WHERE po.status NOT IN (0, 4) AND p.category = '$incoming_category_id' AND c.hashed_id = '$incoming_category_id'
+                                            AND po.warehouse IN ($imploded_warehouse_ids)
+                                            GROUP BY c.hashed_id, p.category
+                                        ";
                                     } else {
                                         $products_query = "
                                         SELECT 
@@ -47,6 +61,18 @@
                                         AND s.warehouse = '$dashboard_wh'
                                         GROUP BY p.category
                                         ";
+
+                                        $po_query = "
+                                            SELECT 
+                                                SUM(poc.qty) AS incoming
+                                            FROM purchased_order_content poc
+                                            LEFT JOIN purchased_order po ON po.id = poc.po_id
+                                            LEFT JOIN product p ON p.hashed_id = poc.product_id
+                                            LEFT JOIN category c ON c.hashed_id = p.category
+                                            WHERE po.status NOT IN (0, 4) AND p.category = '$incoming_category_id' AND c.hashed_id = '$incoming_category_id'
+                                            AND po.warehouse = '$dashboard_wh'
+                                            GROUP BY c.hashed_id, p.category
+                                        ";
                                     }
                                     $product_res = $conn->query($products_query);
                                     if($product_res->num_rows>0){
@@ -54,16 +80,7 @@
                                         $current_stocks = $row['stocks'];
                                     }
 
-                                    $po_query = "
-                                        SELECT 
-                                            SUM(poc.qty) AS incoming
-                                        FROM purchased_order_content poc
-                                        LEFT JOIN purchased_order po ON po.id = poc.po_id
-                                        LEFT JOIN product p ON p.hashed_id = poc.product_id
-                                        LEFT JOIN category c ON c.hashed_id = p.category
-                                        WHERE po.status NOT IN (0, 4) AND p.category = '$incoming_category_id' AND c.hashed_id = '$incoming_category_id'
-                                        GROUP BY c.hashed_id, p.category
-                                    ";
+                            
                                     $po_res = $conn->query($po_query);
                                     if($po_res->num_rows>0){
                                         $row=$po_res->fetch_assoc();
@@ -71,13 +88,11 @@
                                     }
 
                                     echo '<tr>
-                                        <td>' . htmlspecialchars($incoming_category) . '</td>
-                                        <td class="text-end"><a href="../incoming-stocks/?name=' . $incoming_category_id . '&&type=current">' . $current_stocks . '</td>
-                                        <td class="text-end"><a href="../incoming-stocks/?name=' . $incoming_category_id . '&&type=incoming">' . $incoming_qty . '</td>
+                                        <td><a href="../incoming-stocks/?name2=' . $incoming_category . '&&name=' . $incoming_category_id . '&&wh=' . $dashboard_wh . '">' . htmlspecialchars($incoming_category) . '</a></td>
+                                        <td class="text-end">' . $current_stocks . '</td>
+                                        <td class="text-end">' . $incoming_qty . '</td>
                                     </tr>';
                                 }
-                            } else {
-                                echo '<tr><td colspan="5" class="text-center">No incoming stocks found.</td></tr>';
                             }
                             ?>
                         </tbody>
